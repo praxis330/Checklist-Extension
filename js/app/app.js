67409f21ea72440ac677f33591eae9b522788f5a
Nicholas Praxin <praxis@users.noreply.github.com>
2015-08-17 23:16:35 +0100

    Added style sheets

--- js/app/app.js
+++ js/app/app.js
@@ -2,16 +2,19 @@ var checklistApp = angular.module('checklistApp', []);
 
 checklistApp.controller('checklistController', ['$scope', '$http', function($scope, $http) {
 	'use strict';
-	$scope.fetchItems = function ($scope) {
+
+	$scope.items = null;
+
+	$scope.fetchItems = function () {
 		var url = 'https://checklist-stage.herokuapp.com/api/checklist/tasks';
-		$scope.apply( function() {
-			$scope.items = $http.get(url, {
-				headers: { Authorization: 'Basic TEST' }
-			}).then(function(response) {
-				console.log('items', response.data.tasks);
-				return response.data.tasks;
-			});
+		
+		$http.get(url, {
+			headers: { Authorization: 'Basic TEST' }
+		}).then(function(response) {
+			$scope.items = response.data.tasks;
 		});
-	}
+	};
+
+	$scope.fetchItems();
 }]);
 
