3f48fdd7682f7e6e6047baae1d3bb953000edb1e
Nicholas Praxin <praxis@users.noreply.github.com>
2015-08-22 21:53:45 +0100

    Added form to submit checklist item and some polish

--- js/app/app.js
+++ js/app/app.js
@@ -1,20 +1,47 @@
 var checklistApp = angular.module('checklistApp', []);
 
 checklistApp.controller('checklistController', ['$scope', '$http', function($scope, $http) {
-	'use strict';
+  'use strict';
 
-	$scope.items = null;
+  var baseurl = 'https://checklist-stage.herokuapp.com/api/checklist/tasks';
+  var auth_headers = {
+    Authorization: 'Basic '
+  }
 
-	$scope.fetchItems = function () {
-		var url = 'https://checklist-stage.herokuapp.com/api/checklist/tasks';
-		
-		$http.get(url, {
-			headers: { Authorization: 'Basic TEST' }
-		}).then(function(response) {
-			$scope.items = response.data.tasks;
-		});
-	};
+  $scope.items = null;
+  $scope.newItem= {};
 
-	$scope.fetchItems();
+  $scope.fetchItems = function () {
+    $http.get(baseurl, { headers: auth_headers }).then(function(response) {
+      $scope.items = response.data.tasks;
+    });
+  };
+
+  $scope.fetchItems();
+
+  $scope.completeTask = function(taskId) {
+    $http.put(baseurl + '/' + taskId,
+      { 'done': true },
+      { headers: auth_headers}
+    );
+    $scope.fetchItems();
+  };
+
+  $scope.deleteTask = function(taskId) {
+    $http.delete(baseurl + '/' + taskId,
+      { headers: auth_headers }
+    );
+    $scope.fetchItems();
+  };
+
+  $scope.addTask = function() {
+    $scope.newItem.done = false;
+    $http.post(baseurl + '/', 
+      $scope.newItem,
+      { headers: auth_headers }
+    );
+    $scope.fetchItems();
+    $scope.newItem = {};
+  };
 }]);
 
